ba55f931dbca9aac3507fa01ccd9f4bda6242d9a
Joao Antonio <joaoantoniomaruti@gmail.com>
2016-08-27 19:02:50 -0300

    um monte de coisa

--- www/views/admin/admin.js
+++ www/views/admin/admin.js
@@ -3,6 +3,56 @@
 
 angular.module('app.admin', [])
 
-.controller('admin.ctrl', function(){
-    console.log('admin')
+.controller('admin.ctrl', function($scope, $ionicModal){
+    $scope.filtrosAtivos = [];
+
+    /**
+     * Inicializa o modal
+     */
+    $ionicModal.fromTemplateUrl('./views/admin/filtros-admin.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    
+    
+    $scope.filtrar = () => {
+      $scope.modal.show();
+    };
+    
+    
+    $scope.fecharFiltros = () => {
+        $scope.modal.hide();
+    };
+
+
+    $scope.filtros = [
+        {
+            id:1
+            , name:'Informações gerencias'
+        } , {
+            id:2
+            , name:'Novos usuarios'
+        } 
+    ];
+    
+    for(let i = 0; i < $scope.filtros.length; i++){
+      $scope.filtrosAtivos.push($scope.filtros[i]);  
+    }
+    //$scope.filtrosAtivos = $scope.filtros;
+
+    $scope.selecionaFiltro = (filtro) => {
+        var idx = $scope.filtrosAtivos.indexOf(filtro);
+        if(idx > -1){
+            $scope.filtrosAtivos.splice(filtro, 1)
+        }else{
+            $scope.filtrosAtivos.push(filtro);
+        }
+        console.log($scope.filtrosAtivos);
+    }
+    
+    $scope.adicionarFiltros = () => {
+       
+    }
 });
